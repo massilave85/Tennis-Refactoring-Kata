2b5154264f84b1b67ccd6529eabf44f61cf4448f
Massimo Lavelli <lavelli.massimo@gmail.com>
2016-10-12 00:13:45 +0200

    New optimization replacing the List<string> with a matrix

--- csharp/Tennis/TennisGame2.cs
+++ csharp/Tennis/TennisGame2.cs
@@ -10,7 +10,14 @@ namespace Tennis
         private string player1Name;
         private string player2Name;
 
-        private List<string> playerScore = new List<string> { "Love", "Fifteen", "Thirty", "Forty" };
+        private string[,] playerScore = {
+            { "Love-All", "Love-Fifteen", "Love-Thirty", "Love-Forty", "Win for player2", "" },
+            { "Fifteen-Love", "Fifteen-All", "Fifteen-Thirty", "Fifteen-Forty", "Win for player2", "" },
+            { "Thirty-Love", "Thirty-Fifteen", "Thirty-All", "Thirty-Forty", "Win for player2", "" },
+            { "Forty-Love", "Forty-Fifteen", "Forty-Thirty", "Deuce", "Advantage player2", "Win for player2" },
+            { "Win for player1", "Win for player1", "Win for player1", "Advantage player1", "", "" },
+            { "", "", "", "Win for player1", "", "" }
+        };
 
         public TennisGame2(string player1Name, string player2Name)
         {
@@ -22,62 +29,23 @@ namespace Tennis
 
         public string GetScore()
         {
-            string score = "";
-
-            if (p1point >= 4 && p2point >= 0 && (p1point - p2point) >= 2)
-            {
-                score = "Win for player1";
-            }
-            else if (p2point >= 4 && p1point >= 0 && (p2point - p1point) >= 2)
-            {
-                score = "Win for player2";
-            }
-            else if (p1point > p2point && p2point >= 3 && (p1point - p2point) < 2)
-            {
-                score = "Advantage player1";
-            }
-            else if (p2point > p1point && p1point >= 3 && (p2point - p1point) < 2)
-            {
-                score = "Advantage player2";
-            }
-            else if ((p1point < 4 || p2point < 4) && p1point != p2point)
-            {
-                score = playerScore[p1point] + "-" + playerScore[p2point];
-            }
-            else if (p1point == p2point && p1point > 2)
-                score = "Deuce";
-            else
-            {
-                score = playerScore[p1point] + "-All";
-            }
-
-            return score;
-        }
-
-        public void SetP1Score(int number)
-        {
-            for (int i = 0; i < number; i++)
-            {
-                P1Score();
-            }
-        }
-
-        public void SetP2Score(int number)
-        {
-            for (var i = 0; i < number; i++)
-            {
-                P2Score();
-            }
+            return playerScore[p1point, p2point];
         }
 
         private void P1Score()
         {
-            p1point++;
+            if (playerScore[p1point, p2point] == "Advantage player2")
+                p2point--;
+            else
+                p1point++;
         }
 
         private void P2Score()
         {
-            p2point++;
+            if (playerScore[p1point, p2point] == "Advantage player1")
+                p1point--;
+            else
+                p2point++;
         }
 
         public void WonPoint(string player)
